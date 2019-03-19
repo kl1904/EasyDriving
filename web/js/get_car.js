50ba867c2b3c60cb07ed087997521765da48765d
Semjon <1998semi@googlemail.com>
2019-03-19 08:44:58 +0100

    Get_Car zweidimensionale Schleife..

--- web/js/get_car.js
+++ web/js/get_car.js
@@ -1,18 +1,10 @@
 
 var db = firebase.firestore();
-var marke = document.getElementById("marke").value;
-var modell = document.getElementById("modell").value;
-var kraftstoff = document.getElementById("kraftstoff").value;
-var schaltung = document.getElementById("schaltung").value;
-var tueren = document.getElementById("tueren").value;
-var klima = document.getElementById("klima").value;
-var navigationsgeraet = document.getElementById("navigationsgeraet").value;
-var plaetze = document.getElementById("plaetze").value;
-var raucherwagen = document.getElementById("raucherwagen").value;
-var stellplatznummer = document.getElementById("stellplatznummer").value;
 
-var cars = []
-function addUserTableRow(doc) {
+let cars = [];
+idcounter = 0;
+
+function getCarData(doc) {
   //Wenn der DB-Eintrag users existiert
   db.collection("cars").get().then(function(querySnapshot) {
   //Mache/ Führe für alle Elemente die darauf folgenden Sachen aus
@@ -21,18 +13,13 @@ function addUserTableRow(doc) {
           console.log(doc.id, " => ", doc.data());
           //Rufe Funktion addUserTableRow auf
 
-          if (typeof(Storage) !== "undefined") {
-            if (sessionStorage.marke) {
-              sessionStorage.marke = Number(sessionStorage.clickcount)+1;
-            } else {
-              sessionStorage.clickcount = 1;
-            }
-            document.getElementById("result").innerHTML = "You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
-          } else {
-            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-          }
+          cars[idcounter] = [doc.data().marke, doc.data().modell, doc.data().kraftstoff, doc.data().schaltung, doc.data().tueren, doc.data().klima, doc.data().navigationsgeraet, doc.data().pleatze, doc.data().raucherwagen, doc.data().stellplatznummer]
 
-          }
+          idcounter++;
       });
+
   });
+
+console.log(cars);
+
 }
