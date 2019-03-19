74bc6ca037f63ada0c58d4d17d8fb1d0c67607a9
Semjon <1998semi@googlemail.com>
2019-03-19 16:53:05 +0100

    ...

--- web/js/get_car.js
+++ web/js/get_car.js
@@ -6,30 +6,26 @@ function getCarData(doc) {
   idcounter = 0;
   //Wenn der DB-Eintrag users existiert
   db.collection("cars").get().then(function(querySnapshot) {
-  //Mache/ Führe für alle Elemente die darauf folgenden Sachen aus
+  //Mache Führe für alle Elemente die darauf folgenden Sachen aus
       querySnapshot.forEach(function(doc) {
-          //schreiben alle User mit ihren Daten in die Konsole
-          console.log(doc.id, " => ", doc.data());
-          //Rufe Funktion addUserTableRow auf
 
-          cars[idcounter] = [doc.data().marke, doc.data().modell, doc.data().kraftstoff, doc.data().schaltung, doc.data().tueren, doc.data().klima, doc.data().navigationsgeraet, doc.data().pleatze, doc.data().raucherwagen, doc.data().stellplatznummer]
+          cars[idcounter] = [doc.data().marke, doc.data().modell, doc.data().kraftstoff, doc.data().schaltung, doc.data().tueren, doc.data().klima, doc.data().navigationsgeraet, doc.data().raucherwagen, doc.data().stellplatznummer]
 
+          for (i = 0; i < cars[idcounter].length; i++) {
+            var x = document.getElementById(i.toString());
+            var option = document.createElement("option");
+            option.text = cars[idcounter][i];
+            x.add(option);
+          }
           idcounter++;
+
       });
+
+
 
   });
 
 console.log(cars);
-for (i = 0; i <= idcounter.length; i++) {
-  for (j = 0; j <= cars[i].length; j++) {
-    var x = document.getElementById("selectmarke");
-    var option = document.createElement("option");
-    option.text = cars[i][j];
-    x.add(option);
-  }
-
-}
-
 
 
 }
