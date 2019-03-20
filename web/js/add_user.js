c24364bead127c7180790388f0ba2973e51a6b66
Semjon <1998semi@googlemail.com>
2019-03-20 17:10:52 +0100

    Nutzer wird im firebase.auth() erstellt

--- web/js/add_user.js
+++ web/js/add_user.js
@@ -1,17 +1,18 @@
 function addUser(){
   window.alert("WTF");
-  var ref = firebase.database().ref().child('users');
-  ref.set({
-  var displayName = document.getElementById("displayName").value;
-  var userEmail = document.getElementById("email").value;
-  var userPass = document.getElementById("password").value;
 
-})
+  //var displayName = document.getElementById("displayName").value; (displayName gibt es nicht im formaular gucke wie die id heißt)
+  var email= document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
 //firebase funktion für das registrieren von Nutzern (admin)
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
+  window.alert(errorMessage);
+  console.log("nutzer erstellt");
   // ...
 });
+
 }
