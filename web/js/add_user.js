baceb9e6411b7425e8eb0122f9dfe1600192f3cb
Kevin Lederer <kevin.l04@web.de>
2019-03-20 16:47:31 +0100

    nur ein test, da function adduser mit klick auf register nicht aufgerufen wird. (restlicher Code in addUser ist gerade wohl eh noch Müll)

--- web/js/add_user.js
+++ web/js/add_user.js
@@ -1,8 +1,12 @@
 function addUser(){
-
+  window.alert("WTF");
+  var ref = firebase.database().ref().child('users');
+  ref.set({
+  var displayName = document.getElementById("displayName").value;
   var userEmail = document.getElementById("email").value;
   var userPass = document.getElementById("password").value;
 
+})
 //firebase funktion für das registrieren von Nutzern (admin)
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
   // Handle Errors here.
