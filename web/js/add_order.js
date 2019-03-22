c6090bc64a0f43c01d2847606cb6282b71654c44
Florian Hess <floh.hess97@web.de>
2019-03-22 10:20:12 +0100

    wieder lauffähig

--- web/js/add_order.js
+++ web/js/add_order.js
@@ -1,36 +1,39 @@
-function addOrder() {
+function addOrder(){
 
   var idcounter = "0";
   var db = firebase.firestore();
   var orderRef = db.collection("order");
-  var uid = sessionStorage.uid;
+  var vorname = document.getElementById("vorname").value;
+  var name = document.getElementById("name").value;
   var abholdatum = document.getElementById("abholdatum").value;
   var rueckgabedatum = document.getElementById("rueckgabedatum").value;
   var marke = document.getElementById("marke").value;
   var modell = document.getElementById("modell").value;
-  var kraftsstoff = document.getElementById("kraftstoff").value;
+  var kraftstoff = document.getElementById("kraftstoff").value;
   var schaltung = document.getElementById("schaltung").value;
   var tueren = document.getElementById("tueren").value;
   var klima = document.getElementById("klima").value;
   var navigationsgeraet = document.getElementById("navigationsgeraet").value;
+  var plaetze = document.getElementById("plaetze").value;
   var raucherwagen = document.getElementById("raucherwagen").value;
   var stellplatznummer = document.getElementById("stellplatznummer").value;
-  var leihgrund = document.getElementById("leihgrund").value;
+
 
   db.collection("order").get().then(function(querySnapshot) {
   //Mache/ Führe für alle Elemente die darauf folgenden Sachen aus
       querySnapshot.forEach(function(doc) {
-        //schreiben alle Order mit ihren Daten in die Konsole
+        //schreiben alle User mit ihren Daten in die Konsole
         console.log(doc.id, " => ", doc.data());
         idcounter++;
+
       });
 
-      // Antrag in DB anlegen..
+      // Auto in DB anlegen..
       var id = idcounter.toString();
       orderRef.doc(id).set({
-      orderid: idcounter,
-      carid: carid,
-      uid:  uid,
+      rentid: idcounter,
+      vorname: vorname,
+      name: name,
       abholdatum: abholdatum,
       rueckgabedatum: rueckgabedatum,
       marke: marke,
@@ -39,10 +42,15 @@ function addOrder() {
       schaltung: schaltung,
       tueren: tueren,
       klima: klima,
-      navigationsgeraet: navigationsgeraet,
+      navigationsgeraet:navigationsgeraet,
+      plaetze: plaetze,
       raucherwagen: raucherwagen,
-      stellplatznummer: stellplatznummer,
-      leihgrund: leihgrund});
+      stellplatznummer: stellplatznummer});
+
+      //location.replace('admin_carpool_overview.html');
 
   });
+
+
+
 }
