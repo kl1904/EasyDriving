75245bd2f45ddaa8f953f5b5a1c2ecef2ca23b8f
Florian Hess <floh.hess97@web.de>
2019-03-22 10:20:27 +0100

    angepasst

--- web/js/add_car.js
+++ web/js/add_car.js
@@ -30,7 +30,7 @@ function addCar(){
       var id = idcounter.toString();
       carRef.doc(id).set({
       carid: idcounter,
-      rentalid: "none";
+      //rentalid: "none";
       marke: marke,
       modell: modell,
       kraftstoff: kraftstoff,
@@ -42,7 +42,7 @@ function addCar(){
       raucherwagen: raucherwagen,
       stellplatznummer: stellplatznummer});
 
-      location.replace('admin_carpool_overview.html');
+      //location.replace('admin_carpool_overview.html');
 
   });
 
