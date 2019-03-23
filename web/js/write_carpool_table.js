15d4b77ee401f6abdaeda06f1c26f8f4d87b4284
LiebFabian <fabianlieb@web.de>
2019-03-23 21:49:55 +0100

    Anpassungen Carpool

--- web/js/write_carpool_table.js
+++ web/js/write_carpool_table.js
@@ -1,13 +1,13 @@
 
 var db = firebase.firestore();
-//Wenn der DB-Eintrag users existiert
-db.collection("users").get().then(function(querySnapshot) {
+//Wenn der DB-Eintrag cars existiert
+db.collection("cars").get().then(function(querySnapshot) {
 //Mache/ Führe für alle Elemente die darauf folgenden Sachen aus
     querySnapshot.forEach(function(doc) {
         //schreiben alle User mit ihren Daten in die Konsole
         console.log(doc.id, " => ", doc.data());
         //Rufe Funktion addUserTableRow auf
-        addUserTableRow(doc.data());
+        addCarToTableRow(doc.data());
         //Wenn ein Objekt mit dem Classennamen no-records-found existiert setze bei allen das display auf none (unsichtbar)
         if (document.getElementsByClassName("no-records-found")){
             var elems = document.getElementsByClassName('no-records-found');
@@ -19,20 +19,27 @@ db.collection("users").get().then(function(querySnapshot) {
 });
 
 var counter = 0;
-function addUserTableRow(doc) {
+function addCarToTableRow(doc) {
     //Variablen newRow -> neue Datenreihe ; cols -> Zellenininhalte
     var newRow = $("<tr>");
     var cols = "";
 
     cols += '<td class="bs-checkbox "><input data-index="0" name="btSelectItem" type="checkbox"></td>';
-    cols += '<td>'+doc.uid+'</td>';
-    cols += '<td>'+doc.email+'</td>';
-    cols += '<td>'+doc.role+'</td>';
-    cols += '<td>'+doc.photoURL+'</td>';
+    cols += '<td>'+doc.id+'</td>';
+    cols += '<td>'+doc.klima+'</td>';
+    cols += '<td>'+doc.kraftstoff+'</td>';
+    cols += '<td>'+doc.marke+'</td>';
+    cols += '<td>'+doc.modell+'</td>';
+    cols += '<td>'+doc.navigationsgeraet+'</td>';
+    cols += '<td>'+doc.plaetze+'</td>';
+    cols += '<td>'+doc.raucherwagen+'</td>';
+    cols += '<td>'+doc.schaltung+'</td>';
+    cols += '<td>'+doc.stellplatznummer+'</td>';
+    cols += '<td>'+doc.tueren+'</td>';
 
     //Füge die Zelleninhalte in die neue Datenreihe und füge diese in die Tabelle mit der id user_table
     newRow.append(cols);
-    $("table.user_table").append(newRow);
+    $("table.car_table").append(newRow);
 
     counter++;
 }
